e3a26624849f540fcb038afdd5ccf796988eba5a
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2023-09-24 09:04:58 +0700

    fix: fetch all pages not duplicate

--- main.js
+++ main.js
@@ -5,58 +5,39 @@ function addItem(item) {
   card.className = "card";
   card.style.marginBottom = "16px";
 
-  const cardBody = document.createElement("div");
-  cardBody.className = "card-body";
-
-  const cardTitle = document.createElement("h5");
-  cardTitle.className = "card-title";
-  cardTitle.innerHTML = item.name;
-
-  const cardText = document.createElement("p");
-  cardText.className = "card-text";
-  cardText.innerHTML = item.pantone_value;
-
-  const cardColorText = document.createElement("p");
-  cardColorText.className = "card-text";
-  cardColorText.innerHTML = item.color;
-
-  const cardColor = document.createElement("div");
-  cardColor.style.height = "25px";
-  cardColor.style.backgroundColor = item.color;
-
-  cardBody.appendChild(cardTitle);
-  cardBody.appendChild(cardText);
-  cardBody.appendChild(cardColorText);
-  cardBody.appendChild(cardColor);
-  card.appendChild(cardBody);
+  card.innerHTML = `
+    <div class="card-body">
+      <h5 class="card-title">${item.name}</h5>
+      <p class="card-text">${item.pantone_value}</p>
+      <p class="card-text">${item.color}</p>
+      <div style="height: 25px; background-color: ${item.color}"></div>
+    </div>
+  `;
 
   itemsContainer.appendChild(card);
 }
 
 async function fetchColorsList() {
-  // Page 1
   const response = await axios.get("https://reqres.in/api/data");
-  console.log(response);
   const { data } = response;
-  console.log(data.data);
   let allColors = data.data;
-  // Page 2 and others
-  for (let page = data.page; page < data.total_pages; page++) {
+
+  for (let page = data.page + 1; page <= data.total_pages; page++) {
     const response = await axios.get("https://reqres.in/api/data", {
       params: { page: page },
     });
     const { data } = response;
-    data.data.forEach((index) => {
-      allColors.push(index);
-    });
+    allColors = [...allColors, ...data.data];
   }
+
   itemsContainer.innerHTML = "";
   for (let i = 0; i < allColors.length; i++) {
     addItem(allColors[i]);
   }
-  console.log(allColors);
+
   for (let i = 0; i < allColors.length; i++) {
     window.localStorage.setItem(allColors[i].name, allColors[i].color);
   }
 }
+
 fetchColorsList();
