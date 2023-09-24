92ce6ad0c0d7cda97444836bbfa7a21371743881
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2023-09-24 08:52:00 +0700

    feat: use dom method to be more readable

--- main.js
+++ main.js
@@ -1,33 +1,56 @@
 const itemsContainer = document.getElementById("list-items");
 
 function addItem(item) {
-  const itemHTML = '<div class="card" style="width: 18rem;">\n' +
-    '    <div class="card-body">\n' +
-    '        <h5 class="card-title">' + item.name + '</h5>\n' +
-    '        <p class="card-text">' + item.pantone_value + '</p>\n' +
-    '        <div style="background:' + item.color + ';">' + item.color + '</div>\n' +
-    '    </div>\n' +
-    '</div>\n' +
-    '<br/>';
-  itemsContainer.innerHTML += itemHTML;
+  const card = document.createElement("div");
+  card.className = "card";
+  card.style.marginBottom = "16px";
+
+  const cardBody = document.createElement("div");
+  cardBody.className = "card-body";
+
+  const cardTitle = document.createElement("h5");
+  cardTitle.className = "card-title";
+  cardTitle.innerHTML = item.name;
+
+  const cardText = document.createElement("p");
+  cardText.className = "card-text";
+  cardText.innerHTML = item.pantone_value;
+
+  const cardColorText = document.createElement("p");
+  cardColorText.className = "card-text";
+  cardColorText.innerHTML = item.color;
+
+  const cardColor = document.createElement("div");
+  cardColor.style.height = "25px";
+  cardColor.style.backgroundColor = item.color;
+
+  cardBody.appendChild(cardTitle);
+  cardBody.appendChild(cardText);
+  cardBody.appendChild(cardColorText);
+  cardBody.appendChild(cardColor);
+  card.appendChild(cardBody);
+
+  itemsContainer.appendChild(card);
 }
 
 async function fetchColorsList() {
   // Page 1
-  const response = await axios.get('https://reqres.in/api/data');
+  const response = await axios.get("https://reqres.in/api/data");
   console.log(response);
   const { data } = response;
   console.log(data.data);
   let allColors = data.data;
   // Page 2 and others
   for (let page = data.page; page < data.total_pages; page++) {
-    const response = await axios.get('https://reqres.in/api/data', { params: { page: page } });
-    const { data } = response
-    data.data.forEach(index => {
+    const response = await axios.get("https://reqres.in/api/data", {
+      params: { page: page },
+    });
+    const { data } = response;
+    data.data.forEach((index) => {
       allColors.push(index);
     });
   }
-  itemsContainer.innerHTML = '';
+  itemsContainer.innerHTML = "";
   for (let i = 0; i < allColors.length; i++) {
     addItem(allColors[i]);
   }
